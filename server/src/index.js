a0f74c72f1572f449a654778dc746320927323d5
Yash Lalwani <yash.lalwani10@outlook.com>
2025-12-23 20:43:42 -0500

    express server setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,18 @@
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("This is the home route");
+});
+
+app.get("/instagram", (req, res) => {
+  res.send("this is the instgram route");
+});
+
+app.listen(port, () => {
+  console.log(`app is listening to port ${port}`);
+});
